1ad5e7d7b32cb6559283b31934c847e843d0787f
Jinsung-L <js99028@gmail.com>
2018-08-05 20:03:06 +0900

    Rename package to `sitmango`
    This resolves installation issue on some environments.

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 setup(
     name='sitmango',
-    version='0.1.0',
+    version='0.1.1',
     description='Simple deployment for Flask micro web service',
     url='https://github.com/Jinsung-L/sit/',
     author='Jinsung Lim',
     author_email='js99028@gmail.com',
     license='MIT',
     packages=[
-        'sit',
-        'sit.scripts',
+        'sitmango',
+        'sitmango.scripts',
     ],
     entry_points = {
         'console_scripts': ['sit=sit.scripts.sit:cli'],
